0f2c165ea1a71b84cbd37d58c5bb85c17ab7d3f6
euzebe <euzebe@axiome.me>
2019-01-14 21:31:31 +0100

    add chronometer + tests

--- src/components/Chronometer.spec.tsx
+++ src/components/Chronometer.spec.tsx
@@ -6,13 +6,61 @@ import "react-testing-library/cleanup-after-each";
 import { render } from "react-testing-library";
 import Chronometer from "./Chronometer";
 
+beforeEach(() => {
+  jest.useFakeTimers();
+});
 
 describe("Chronometer", () => {
   it("should measure time elapsed between startTime and the moment the Stop button is clicked", () => {
-    render(<Chronometer startTime={new Date()}/>);
+    const r = render(<Chronometer />);
+    r.getByTestId("play").click();
+    jest.advanceTimersByTime(12003);
+    expect(r.getByTestId("display")).toHaveTextContent("00:12");
   });
 
-  it('should not count time during which chronometer was stopped', () => {
+  it("should display hours when more than an hour elapsed", () => {
+    const r = render(<Chronometer />);
+    r.getByTestId("play").click();
+    jest.advanceTimersByTime(3603400);
+    r.getByTestId("stop").click();
+    expect(r.getByTestId("display")).toHaveTextContent("01:00:03");
+  });
 
+  it("should invoke callback when stop button is clicked, and should not count elapsed time after", () => {
+    const chronoStopped = jest.fn();
+    const r = render(<Chronometer onStop={chronoStopped} />);
+    r.getByTestId("play").click();
+    jest.advanceTimersByTime(12003);
+    expect(chronoStopped).not.toHaveBeenCalled();
+
+    r.getByTestId("stop").click();
+
+    jest.advanceTimersByTime(12312003);
+    expect(r.getByTestId("display")).toHaveTextContent("00:12");
+    expect(chronoStopped).toHaveBeenCalledTimes(1);
+    expect(chronoStopped).toHaveBeenCalledWith(12000);
+  });
+
+  it("should call stop callback on unmount if chrono is playing", () => {
+    const chronoStopped = jest.fn();
+    const notLaunched = render(<Chronometer onStop={chronoStopped} />);
+    jest.advanceTimersByTime(300);
+    notLaunched.unmount();
+    expect(chronoStopped).not.toHaveBeenCalled();
+
+    const r = render(<Chronometer onStop={chronoStopped} />);
+    r.getByTestId("play").click();
+    jest.advanceTimersByTime(12300);
+    r.unmount();
+    expect(chronoStopped).toBeCalledWith(12300);
+  });
+
+  it("should display Play button when chronometer is paused, Pause button else", () => {
+    const r = render(<Chronometer />);
+    expect(r.queryByTestId("play")).toBeInTheDocument();
+    expect(r.queryByTestId("pause")).toBeNull();
+    r.getByTestId("play").click();
+    expect(r.queryByTestId("play")).toBeNull();
+    expect(r.queryByTestId("pause")).toBeInTheDocument();
   });
 });
--- src/components/Chronometer.tsx
+++ src/components/Chronometer.tsx
@@ -1,42 +1,56 @@
 import * as React from "react";
 
-// TODO: add hideStop, hidePlayPause, deal with PlayPause, callback when stopped (with time, callback when playPaused (with actual status and time)
+// TODO: add hideStop, hidePlayPause, deal with PlayPause, callback when stopped (with time), hide all buttons when stopped, callback when playPaused (with actual status and time)
 
 interface ChronometerProps {
-  startTime: Date;
   onStop?: (duration: number) => void;
 }
 
 interface ChronometerState {
-  elapsedTime?: number;
+  elapsedTime: number;
+  pauseStartTime?: number;
+  pausedTime?: number;
   interval?: any;
+  isPlaying: boolean;
 }
 
+const STEP_TIME = 100;
+
 class Chronometer extends React.PureComponent<ChronometerProps> {
-  state: ChronometerState = {};
+  state: ChronometerState = { elapsedTime: 0, isPlaying: false };
 
   componentWillUnmount() {
     this.stop();
   }
 
-  componentDidUpdate(props: ChronometerProps, prevState: ChronometerState) {
-    if (props.startTime && !prevState.interval) {
-      const interval = setInterval(() => {
-        const elapsedTime = new Date().getTime() - props.startTime.getTime();
-        this.setState({ elapsedTime });
-      }, 1000);
-      this.setState({ interval });
-    }
-  }
+  play = () => {
+    const interval = setInterval(() => {
+      const elapsedTime = this.state.elapsedTime + STEP_TIME;
+      this.setState({ elapsedTime });
+    }, STEP_TIME);
+    this.setState({ interval, isPlaying: true });
+  };
 
   stop = () => {
     const { interval, elapsedTime } = this.state;
     const { onStop } = this.props;
     if (interval) {
       clearInterval(interval);
-      this.setState({ interval: null });
+      this.setState({ interval: null, isPlaying: false });
       if (onStop && elapsedTime) onStop(elapsedTime);
     }
+  };
+
+  pause = () => {
+    const { interval } = this.state;
+    if (interval) {
+      clearInterval(interval);
+      this.setState({ interval: null, isPlaying: false });
+    }
+  };
+
+  reset = () => {
+    this.setState({ elapsedTime: 0 });
   };
 
   onTwoDigits = (value: number) => value.toString().padStart(2, "0");
@@ -51,14 +65,30 @@ class Chronometer extends React.PureComponent<ChronometerProps> {
       elapsedTimeAsDate.getSeconds()
     ]
       .map(this.onTwoDigits)
+      .filter((d, index) => index !== 0 || d !== "00")
       .join(":");
   }
 
   render() {
+    const { isPlaying } = this.state;
     return (
       <React.Fragment>
-        <h2>{this.displayElapsedTime()}</h2>
-        <button onClick={this.stop}>⬛</button>
+        <h2 data-testid="display">{this.displayElapsedTime()}</h2>
+        {isPlaying ? (
+          <button onClick={this.pause} data-testid="pause">
+            ⏸️
+          </button>
+        ) : (
+          <button onClick={this.play} data-testid="play">
+            ▶
+          </button>
+        )}
+        <button onClick={this.stop} data-testid="stop">
+          ⬛
+        </button>
+        <button onClick={this.reset} data-testid="reset">
+          🔃
+        </button>
       </React.Fragment>
     );
   }
