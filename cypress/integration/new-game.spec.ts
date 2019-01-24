8a816c012d114b8078baae06ec80cecd3fe1bf22
euzebe <euzebe@axiome.me>
2019-01-24 09:00:00 +0100

    tests

--- cypress/integration/new-game.spec.ts
+++ cypress/integration/new-game.spec.ts
@@ -1,7 +1,9 @@
+/// <reference types="cypress" />
 // this adds custom jest matchers from jest-dom
 describe("new game", () => {
   it('should be accessible from /game', () => {
-    cy.visit('/game');
+    cy.visit('/');
+    cy.getByText(/Nouvelle partie/i).click();
     cy.queryAllByTestId("picked").should("be.empty");
     cy.getByText("42").click();
     cy.getByText("29").click();
